930fd64d070a10b911d84b67634aaf0b311c18fb
TH-c-o-d-e <165408638+TH-c-o-d-e@users.noreply.github.com>
2024-04-26 18:29:35 +0200

    Mise en forme

--- myapp/test/model.test.js
+++ myapp/test/model.test.js
@@ -1,22 +1,23 @@
-const DB= require ("../model/connexion_bd.js");
-const model= require ("../model/Utilisateur.js");
+const DB = require("../model/connexion_bd.js");
+const model = require("../model/Utilisateur.js");
 describe("Model Tests", () => {
-beforeAll(() => {
-// des instructions à exécuter avant le lancement des tests
-});
-afterAll((done) => {
-function callback (err){
-if (err) done (err);
-else done();
-}
-DB.end(callback);
-});
-test ("read user",()=>{
-nom=null;
-function cbRead(resultat){
-nom = resultat[0].nom;
-expect(nom).toBe("test");
-}
-model.read("test@test.fr", cbRead);
-});
+    beforeAll(() => {
+        // des instructions à exécuter avant le lancement des tests
+    });
+
+    afterAll((done) => {
+        function callback(err) {
+            if (err) done(err);
+            else done();
+        }
+        DB.end(callback);
+    });
+    test("read user", () => {
+        nom = null;
+        function cbRead(resultat) {
+            nom = resultat[0].nom;
+            expect(nom).toBe("test");
+        }
+        model.read("test@test.fr", cbRead);
+    });
 })
